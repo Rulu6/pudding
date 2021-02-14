9d90804b5279b7d8e0cb56b6d6981ce45fdab473
Rulu6 <78897389+Rulu6@users.noreply.github.com>
2021-02-15 08:03:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 import random
+import discord
 
 bot = commands.Bot(command_prefix='p/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,6 +13,11 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+@client.event
+async def on_ready(): 
+    print('製作中...')
+    await client.change_presence(activity=discord.Game(name="with discord.py", type=1))
 
 mylist = ["当たり！", "ハズレ！", "残念！", "ハズレ", "残念", "ハズレ～"]
 
