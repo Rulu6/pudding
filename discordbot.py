92049c4c71a14ab5df8948bce6bbe6f5bc4d2fbd
Rulu6 <78897389+Rulu6@users.noreply.github.com>
2021-02-15 08:13:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,11 +7,11 @@ bot = commands.Bot(command_prefix='p/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
 
 
 mylist = ["当たり！", "ハズレ！", "残念！", "ハズレ", "残念", "ハズレ～"]
